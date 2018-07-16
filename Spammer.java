76ab0ab8bf3d0250a9dec6bf76c7582276a468de
unobatbayar <unobatbayar@users.noreply.github.com>
2018-07-17 04:16:38 +0900

    better varible name

--- Spammer.java
+++ Spammer.java
@@ -10,17 +10,17 @@ Date 22/May/2018
 class Spammer{
     public static void main (String[] args){
 
-        String spammessage = JOptionPane.showInputDialog("What message would you want to spam?");
+        String spam_message = JOptionPane.showInputDialog("What message would you want to spam?");
         int limit = Integer.parseInt(JOptionPane.showInputDialog("How many times?"));
         
         int counter = 0;
-        spam(spammessage, counter, limit);
+        spam(spam_message, counter, limit);
     }
-    public static void spam(String spammessage, int counter, int limit){
+    public static void spam(String spam_message, int counter, int limit){
         if(counter <= limit){
-            System.out.println(spammessage + " " + counter);
+            System.out.println(spam_message + " " + counter);
             counter = counter + 1;
-            spam(spammessage, counter, limit);
+            spam(spam_message, counter, limit);
 
         }
     }
