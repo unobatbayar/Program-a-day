22e2e6c0237bd8a8db4cca40cf719cf33209a642
Uno <38866249+unobatbayar@users.noreply.github.com>
2018-07-17 04:22:21 +0900

    progress

--- Quicksort.java
+++ Quicksort.java
@@ -1,34 +1,35 @@
 import java.util.*;
 
 /*
-AUTHOR: uno
-Program number 4/365
+@author uno
+Program number 4
 23/May/2018
-							Quicksort Algorithm
+							Quicksort Algorithm // Not finished
 					Let's sort user input numbers using quicksort algorithm
 */
 class Quicksort
 {
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
 
-	int[] userinput = new int[5];
-	getinput(userinput);
+		int[] userinput = new int[5];
+		getinput(userinput);
 
 	}
 
-	public static void getinput(int[] userinput){
+	public static void getinput(int[] userinput)
+	{
+		Scanner s = new Scanner(System.in);
 
-	Scanner s = new Scanner(System.in);
-
-	for(int i =0; i<5;i++){
-		System.out.println("Please input random numbers one by one " + i + "/5");
-		userinput[i] = s.nextInt();
+		for(int i =0; i<5;i++)
+		{
+			System.out.println("Please input random numbers one by one " + i + "/5");
+			userinput[i] = s.nextInt();
 		}
-	for(int i: userinput){ //for printing array
-
-            System.out.println(i);
-
-        }
+		for(int i: userinput)
+		{ //for printing array
+	            	System.out.println(i);
+        	}
 	}
 }
 
