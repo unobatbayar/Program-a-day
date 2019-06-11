910efe0646d3326b53221406693a2fa26024b9f1
unobatbayar <uno02bt@gmail.com>
2019-06-11 23:34:03 +0100

    automatic rounder

--- roundToNearest.java
+++ roundToNearest.java
@@ -1,12 +1,12 @@
-
+import javax.swing.JOptionPane;
 class roundToNearest { 
   public static void main(String args[]){
     int n = Integer.parseInt(JOptionPane.showInputDialog("Which number to round?"));
-    int n = Integer.parseInt(JOptionPane.showInputDialog("Which number to round?"));
+    int r = Integer.parseInt(JOptionPane.showInputDialog("Round to nearest?"));
+    System.out.println(round(n, r));
   }
 
-  public int round(int n, int r){
-    int rounded = 0;
-    return rounded;
+  public static int round(int n, int r){
+    return (n + 4)/r * r;
   }
 }
