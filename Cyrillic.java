d4b747338c9e7ed3efb703427b8a370315abb9c0
unobatbayar <uno@konple.com>
2020-03-14 22:58:11 +0800

    feat: add Cyrillic checker program

---
+++ Cyrillic.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/*
+@author unobatbayar
+Cyrillic script is a writing system used for various languages. This program
+recognises if text is using cyrillic letters or not. Tuned for Mongolian cyrillic.
+*/
+class Cyrillic{
+    public static void main (String[] args){
+        System.out.println(isCyrillic("Education")); 
+        System.out.println(isCyrillic("Боловсрол"));
+    }
+
+    public static boolean isCyrillic(final String text) {
+        for(int i = 0; i<text.length(); i++){
+            Pattern pattern;
+            Matcher matcher;
+            final String PASSWORD_PATTERN = "^(?=.*[АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯӨҮабвгдеёжзийклмнопрстуфхцчшщъыьэюяөү]).{1,}$";
+            pattern = Pattern.compile(PASSWORD_PATTERN);
+            matcher = pattern.matcher(text.charAt(i) + "");
+            if(!matcher.matches()){
+                return false;
+            }
+        }
+        return true;
+    }
+}
